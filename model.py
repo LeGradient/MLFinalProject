a9893782661cf08f3efce993af91fa7b0c1ca661
Alexander Seto <git@seto.io>
2017-06-07 15:08:39 -0700

    break everything.

--- model.py
+++ model.py
@@ -14,27 +14,32 @@ class Data:
     def separate_matrix(self, matrix):
         x = matrix[:,:-5]
         y = matrix[:,-5:]
-        #print y[:,0]
         return x, y
 
     def cfs_train(self, l=0):
         ''' closed form solution training '''
-        self.w = [calculate_weights(self.x, self.y[:,i], l) for i in range(5)]
+        self.w = np.array([calculate_weights(self.x, self.y[:,i], l) for i in range(5)])
 
-    def evaluate(self, year, ownership, region, gender):
+    def evaluate(self, year, ownership, gender, region):
         #x_new = [year, gender] + list(map(int, "{0:031b}".format(2**i))) + [1, 0, 0, 0, 0]
-        x_news = [[year, ownership, gender, 1]+one_hot_regions(region) + list(map(int,"{0:05b}".format(2**(i)))) for i in range(5)]
-        print x_news
-        
+        print one_hot_regions(region)
+        #x_news = [[year, ownership, gender, 1]+one_hot_regions(region) + list(map(int,"{0:05b}".format(2**(4-i)))) for i in range(5)]
+        x_new = [year, ownership, gender, 1]+one_hot_regions(region)
+
+        print np.array(x_new[0]).dot(np.array(self.w)[:,0])        
 
 def calculate_weights(x, y, l):
     return np.dot(np.linalg.inv(np.dot(x.T,x) + l*np.identity(len(x[0]))), x.T.dot(y)) 
-
+    #return np.dot( np.linalg.inv(x.T.dot(x))  ,x.T.dot(y))
+    
 def one_hot_regions(region_id):
     return list(map(int, "{0:031b}".format(2**int(region_id))))
 
 
+
 stuff = Data(data.matrix)
+print stuff.x.shape, stuff.y[:,0].shape
+print calculate_weights(stuff.x, stuff.y[:,0], 0)
 stuff.cfs_train(0)
-print stuff.w[0]
-stuff.evaluate([])
+#print stuff.w[0]
+stuff.evaluate(2016, 1, 1, 13)
