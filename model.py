3a96691156b7cbc8a28e69df89bed1ddac6d7e72
Alexander Seto <git@seto.io>
2017-06-07 13:33:07 -0700

    fully implement one hot and ridge regression.

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import data
 '''
 format as:
 [[year, ownership, gender, isRegion0, isRegion1, ..., isRegion_k, pass, d1rate, ... d4rate], ...]
@@ -18,19 +18,17 @@ class Data:
 
     def cfs_train(self, l=0):
         ''' closed form solution training '''
-        self.w = calculate_weights(self.x, self.y, l)
+        self.w = [calculate_weights(self.x, self.y[i], l) for i in range(5)]
 
     def evaluate(self, year, ownership, region, gender):
-        x_new = [year, gender] + [''' 1HOT region'''] + [1, 0, 0, 0, 0]
-        x_news = [ [year, gender] + [''' 1HOT1'''] + list(map(int,"{0:05b}".format(2**(4-i)))) for i in range(5) ]
+        #x_new = [year, gender] + list(map(int, "{0:031b}".format(2**i))) + [1, 0, 0, 0, 0]
+        x_news = [[year, ownership, gender]+list(map(int, "{0:031b}".format(2**region))) + list(map(int,"{0:05b}".format(2**(i)))) for i in range(5)]
         print x_news
         
 
 def calculate_weights(x, y, l):
     return np.dot(np.linalg.inv(np.dot(x.T,x) + l*np.identity(len(x[0]))), x.T.dot(y)) 
 
+def one_hot_regions(region_id):
+    return 
 
-test = np.random.rand(40,30)
-testdata = Data(test)
-print testdata.x
-print testdata.evaluate()
