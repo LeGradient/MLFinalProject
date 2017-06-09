6eee1f8cd3ca5332b226177871d4794025bcf9a0
Alexander Seto <git@seto.io>
2017-06-09 10:44:57 -0700

    add PEP8 compliance and clean old code.

--- model.py
+++ model.py
@@ -1,49 +1,55 @@
 import numpy as np
+import math
 import data
 '''
 format as:
-[[year, ownership, gender, 1, isRegion0, isRegion1, ..., isRegion_k, pass, d1rate, ... d4rate], ...]
+[[year, ownership, gender, 1, region0, ..., region_k, r, d1r, ..., d4r], ...]
 '''
 
 
 class Data:
     def __init__(self, data):
         self.x, self.y = self.separate_matrix(data)
-        
 
     def separate_matrix(self, matrix):
-        x = matrix[:,:-5]
-        y = matrix[:,-5:]
+        x = matrix[:, :-5]
+        y = matrix[:, -5:]
         return x, y
 
-    def cfs_train(self, l=0):
+    def cfs_train(self, l=10e-6):
         ''' closed form solution training '''
-        self.w = np.array([calculate_weights(self.x, self.y[:,i], l) for i in range(5)])
+        self.w = [calculate_weights(self.x, self.y[:, i], l) for i in range(5)]
 
     def evaluate(self, year, ownership, gender, region):
-        #x_new = [year, gender] + list(map(int, "{0:031b}".format(2**i))) + [1, 0, 0, 0, 0]
-        #print one_hot_regions(region)
-        #x_news = [[year, ownership, gender, 1]+one_hot_regions(region) + list(map(int,"{0:05b}".format(2**(4-i)))) for i in range(5)]
         x_new = [year, ownership, gender, 1]+one_hot_regions(region)
+        return [np.array(x_new).dot(self.w[i]) for i in range(5)]
 
-        return np.array(x_new).dot(self.w[0])        
+    def eval(self):
+        return [np.inner(self.w[i], self.x) for i in range(5)]
+
+    def mse(self, data=None):
+        if data is None:
+            data = self
+        return [np.mean((data.y[:, i] - self.eval()[i])**2) for i in range(5)]
+
 
 def calculate_weights(x, y, l):
-    #return np.dot(np.linalg.inv(np.dot(x.T,x) + l*np.identity(len(x[0]))), x.T.dot(y)) 
-    return np.dot( np.linalg.inv(x.T.dot(x)+np.eye(x.shape[1])*0.0000001)  ,x.T.dot(y))
-    
+    return np.dot(np.linalg.inv(x.T.dot(x)+np.eye(x.shape[1])*l), x.T.dot(y))
+
+
 def one_hot_regions(region_id):
     return list(map(int, "{0:031b}".format(2**int(region_id))))
 
 
-
 stuff = Data(data.matrix)
-#print stuff.x.shape, stuff.y[:,0].shape
-#print calculate_weights(stuff.x, stuff.y[:,0], 0)
-stuff.cfs_train(0)
+# print stuff.x.shape, stuff.y[:,0].shape
+# print calculate_weights(stuff.x, stuff.y[:,0], 0)
+stuff.cfs_train()
 print stuff.w[0]
 print stuff.w[1]
 print stuff.w[2]
 print stuff.w[3]
 print stuff.w[4]
-print stuff.evaluate(2011, 0, 1, 0)
+print "Test Data Point:", stuff.evaluate(2011, 0, 1, 0)
+print "MSE: ", stuff.mse()
+print "RMSE: ", [math.sqrt(stuff.mse()[i]) for i in range(5)]
