6d768e31b9687a12b4b5bf9adfca26f227a4eb40
Alexander Seto <git@seto.io>
2017-06-16 15:23:35 -0700

    Add statistical improvements

--- model.py
+++ model.py
@@ -14,20 +14,31 @@ class Data:
     def separate_matrix(self, matrix):
         x = matrix[:, :-5]
         print x.shape
-        x = np.insert(x, 1, x[:, 0]**2, axis=1)
-        print x.shape
-        print x
-        # print x[:,0]
+        x_new = np.insert(x, 0, x[:, 0]*x[:, 0:34].T, axis=1)
+        #
+        # Hadamard product:
+        #
+        # a1   b11 b12     a1b11 a1b12
+        # a2   b21 b22   = a2b21 a2b22
+        # a3   b31 b32     a3b31 a3b32
+        #
+        x_new = np.insert(x_new, 0, x[:, 1]*x[:, 1:34].T, axis=1)
+        x_new = np.insert(x_new, 0, x[:, 2]*x[:, 2:34].T, axis=1)
         y = matrix[:, -5:]
-        return x, y
+        return x_new, y
 
     def cfs_train(self, l=10e-6):
         ''' closed form solution training '''
         self.w = [calculate_weights(self.x, self.y[:, i], l) for i in range(5)]
 
     def evaluate(self, year, ownership, gender, region):
-        x_n = [year, year, ownership, gender, 1]+one_hot_regions(region)
-        return [np.array(x_n).dot(self.w[i]) for i in range(5)]
+        x = np.array([year, ownership, gender, 1]+one_hot_regions(region))
+        # print x.shape
+        x_new = np.insert(x, 0, x[0]*x[0:34])
+        x_new = np.insert(x_new, 0, x[1]*x[1:34])
+        x_new = np.insert(x_new, 0, x[2]*x[2:34])
+        print x_new.shape
+        return [np.array(x_new).dot(self.w[i]) for i in range(5)]
 
     def eval(self):
         return [np.inner(self.w[i], self.x) for i in range(5)]
